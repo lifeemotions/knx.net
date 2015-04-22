b59f7d883dd38799c43b84c3e2bbe9c6ba59b5be
David Cumps <david@cumps.be>
2015-04-22 10:16:35 +0200

    Fixed documentation generation

--- src/KNXLib/Properties/AssemblyInfo.cs
+++ src/KNXLib/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("KNXLib")]
 [assembly: AssemblyProductAttribute("KNX.net")]
 [assembly: AssemblyDescriptionAttribute("KNX.net provides a KNX API for .NET")]
-[assembly: AssemblyVersionAttribute("1.0")]
-[assembly: AssemblyFileVersionAttribute("1.0")]
+[assembly: AssemblyVersionAttribute("1.0.2")]
+[assembly: AssemblyFileVersionAttribute("1.0.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0";
+        internal const string Version = "1.0.2";
     }
 }
