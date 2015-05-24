da4517721d51c67ed576667fd6bc20462ed543cc
Abel Silva <abel.silva@gmail.com>
2015-05-24 20:21:36 +0100

    nuget package creation

--- src/KNXLib/Properties/AssemblyInfo.cs
+++ src/KNXLib/Properties/AssemblyInfo.cs
@@ -1,18 +1,22 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KNXLib")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KNXLib")]
+[assembly: AssemblyTitle("KNX.net")]
+[assembly: AssemblyDescription("KNX.net provides a KNX API for .NET, providing Tunneling and Routing connection modes.")]
+[assembly: AssemblyCompany("life.emotions")]
+[assembly: AssemblyProduct("KNX.net")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
