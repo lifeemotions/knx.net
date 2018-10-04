83437c1277a43b6b0b4a4639846daf111786a7ef
Marcel Mueller <marcel-mueller@gmx.de>
2018-10-04 10:17:12 +0200

    Fix line-endings

--- src/KNXLib/KnxLockManager.cs
+++ src/KNXLib/KnxLockManager.cs
@@ -39,7 +39,8 @@
 
         public void PerformLockedOperation(Action action)
         {
-            // TODO: Shouldn't this check if we are connected?
+            if (!_isConnected)
+                throw new InvalidOperationException("Unable to perform action: KNX is not connected.");
 
             try
             {
